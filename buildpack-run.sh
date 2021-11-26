6c6b7ff797527760406b2c8daadb7382a33d6d71
Alexandre Beltrán <genesisbm88@gmail.com>
2021-11-26 12:14:53 +0100

    fix permisson denied

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,7 +1,7 @@
 set -e
 
 echo "admin's password: $(cat "$ENV_DIR/ADMIN_PASSWORD")"
-
+chmod +x buildpack-run.sh
 curl "https://www.redmine.org/releases/redmine-$(cat "$ENV_DIR/REDMINE_VERSION").tar.gz" | tar xz --strip 1
 
 mv overlays/config/* config
